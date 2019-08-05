7ad86e699263006daa53b0e23d1a85f85bec52de
Samuel-Piedra58 <samuel58piedra@gmail.xom>
2019-08-04 17:51:53 -0700

    Updated scripts.js/styles.css
    Refactored for modal window display and functionality. Included snippet to format birthday text when modal window is clicked. Added background image and css gradient.

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,35 @@
     Treehouse Techdegree Project 05: Public API Requests
     @author: Samuel Piedra
     @created: 07/27/2019
-    @github: 
+    @github: https://github.com/Samuel-Piedra58/techdegree-project-05
 */
 
-// GLOBAL VARIABLES ==============================
+/*
+    @ Script Description: This js script provides interactivity in the following ways:
+      1. Generates 12 random users, via randomuser.me,  and displays their picture and info.
+      2. Provides the functionality to display a single user in a modal window when that user's card is clicked.
+      3. While displaying a user via the modal window you can toggle back and forth between all VISIBLE users.
+      4. Dynamic search feature that filters visible cards on input based off of the given search term.
+*/
+
+// API URL ====================================
 
 const usersUrl = "https://randomuser.me/api/?results=12&nat=us";
 
-// FETCH FUNCTIONS ==============================
+// FETCH HANDLER ====================================
 
-function getDataFromServer(url) {
-  return fetch(url).then(response => response.json());
+function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  } else {
+    return Promise.reject(response);
+  }
 }
 
-// HANDLER FUNCTIONS ==============================
+// ADDITIONAL HANDLER FUNCTIONS ====================================
 
 function generateGalleryCards(data) {
+  // Function will create and display the cards with each user returned from randomuser response
   const gallery = document.getElementById("gallery");
   const people = data
     .map(person => {
@@ -45,51 +58,52 @@ function generateGalleryCards(data) {
 }
 
 function attachCardListener(data) {
-  // attach an event listener to all the cards
+  // attachs an event listener to all the cards to handle modal window operations if clicked
   const cards = Array.from(document.querySelectorAll("div.gallery .card"));
+
   cards.forEach((card, cardIndex, cards) => {
-    // add data-id attribute to keep track of each card (for maintaining context after cards are filtered)
+    // set data-id attribute for each card
     card.setAttribute("data-id", `${cardIndex}`);
 
-    // set value for card index currently displayed in the modal...
-    // this value will decrement by 1 when the "prev" button is clicked
-    // and increment by 1 when the next card is clicked
-    let cardIndexDisplayed = card.dataset.id;
-
     card.addEventListener("click", function() {
-      // render the modal window
+      // render the modal window when the card is clicked
       const modalWindow = openModalWindow();
 
       // dispaly the info of the employee card that was clicked
       modalWindow.info.innerHTML = buildModalCardInfo(data[cardIndex]);
 
       // attach event listener to modal window close button
-      modalWindow.close.addEventListener("click", closeModalWindow);
+      modalWindow.closeButton.addEventListener("click", closeModalWindow);
+
+      // Capture only visible cards displayed and then catpure those cards "dataset id" values
+      const visibleCardIds = cards
+        .filter(card => {
+          return card.style.display !== "none";
+        })
+        .map(card => {
+          return card.dataset.id;
+        });
+
+      // variable will help maintain the state of the current index card displaying in the modal window
+      // this state will help keep a reference for when we toggle to the previous and next employee
+      let displayingCardIndex = visibleCardIds.indexOf(card.dataset.id);
 
       // attach event listener to modal window prev button
-      modalWindow.prev.addEventListener("click", () => {
-        const availableCards = cards.filter(card => {
-          return card.style.display !== "none";
-        });
-        // I NEED A WAY TO REESTABLISH THE CONTEXT OF AVAILABLE CARDS TO "NAVIGATE" THROUGH
-        // CURRENTLY I AM TOGGLING BACK AND FORTH BETWEEN THE INITIAL CARDS DISPLAYED
-        // BUT I NEED TO TOGGLE BETWEEN ONLY THOSE CARDS WHOSE DISPLAY PROPERTY IS NOT "NONE"
-        // THE CHALLENGE I AM FACING IS THAT I DO NOT KNOW HOW TO TIE THE CURRENT MODAL CARD
-        // AND IT'S INDEX BACK TO THE NEW ARRAY?
-        if (cardIndexDisplayed > 0) {
-          cardIndexDisplayed -= 1;
+      modalWindow.prevButton.addEventListener("click", () => {
+        if (displayingCardIndex - 1 >= 0) {
+          displayingCardIndex -= 1;
           modalWindow.info.innerHTML = buildModalCardInfo(
-            data[cardIndexDisplayed]
+            data[visibleCardIds[displayingCardIndex]]
           );
         }
       });
 
-      // attach event listener to modal window prev button
-      modalWindow.next.addEventListener("click", () => {
-        if (cardIndexDisplayed < cards.length - 1) {
-          cardIndexDisplayed += 1;
+      // attach event listener to modal window next button
+      modalWindow.nextButton.addEventListener("click", () => {
+        if (displayingCardIndex + 1 < visibleCardIds.length) {
+          displayingCardIndex += 1;
           modalWindow.info.innerHTML = buildModalCardInfo(
-            data[cardIndexDisplayed]
+            data[visibleCardIds[displayingCardIndex]]
           );
         }
       });
@@ -97,11 +111,11 @@ function attachCardListener(data) {
   });
 }
 
+// MODAL WINDOW HANDLERS ====================================
+
 function openModalWindow() {
-  // function will be called when a user clicks an employee card
-  // the function will build a modal window and return an object
-  // the object represents all of the elements in the modal window
-  // we will want to interact with later
+  // function will be called when a user clicks an employee card the function will
+  // build a modal window and return an object representing the componenets of the modal window
   const modalWindow = getModalWindow();
   const modalHTML = `
       <div class="modal">
@@ -119,9 +133,9 @@ function openModalWindow() {
 
   return {
     info: document.querySelector(".modal-info-container"),
-    close: document.querySelector("#modal-close-btn"),
-    prev: document.querySelector("#modal-prev"),
-    next: document.querySelector("#modal-next")
+    closeButton: document.querySelector("#modal-close-btn"),
+    prevButton: document.querySelector("#modal-prev"),
+    nextButton: document.querySelector("#modal-next")
   };
 }
 
@@ -152,13 +166,15 @@ function buildModalCardInfo(employee) {
     <p class="modal-text cap">${employee.location.city}</p>
     <hr>
     <p class="modal-text">${employee.phone}</p>
-    <p class="modal-text">
+    <p class="modal-text cap">
         ${employee.location.street},
         ${employee.location.city},
         ${employee.location.state}
         ${employee.location.postcode}
     </p>
-    <p class="modal-text">Birthday: ${employee.dob.date}</p>
+    <p class="modal-text">Birthday: 
+      ${new Date(employee.dob.date).toLocaleDateString()}
+    </p>
     `;
 }
 
@@ -172,13 +188,20 @@ function closeModalWindow(event) {
   }
 }
 
+// SEACH BAR HANDLERS ====================================
+
 function handleSeachFeature() {
-  // Function will fire an event when input changes and will test the name value against the search term
+  // Function will add an event listener to dynamically filter
+  // employee cards based off of applicable search characters
   const searchBar = displaySearchFeature();
+
+  // Get the reference for all the cards displayed on the page
   const cards = Array.from(document.querySelectorAll("div.gallery .card"));
+
   searchBar.addEventListener("input", event => {
     const searchValue = event.target.value;
     const regex = new RegExp(searchValue, "i");
+
     cards.forEach(card => {
       const name = card.querySelector("#name").textContent;
       if (!regex.test(name)) {
@@ -191,6 +214,7 @@ function handleSeachFeature() {
 }
 
 function displaySearchFeature() {
+  // Function will build the search feature and return an HTMLElement referring to the search input
   const searchContainer = document.querySelector(".search-container");
   const searchHTML = `
     <form action="#" method="get">
@@ -202,12 +226,29 @@ function displaySearchFeature() {
   return document.querySelector("#search-input");
 }
 
-// EVENT LISTENERS ==============================
+// CUSTOM ERROR MESSAGE ====================================
+
+function renderErrorMessage() {
+  // If the promise from FETCH Api is rejected, this simple error message will display
+  const gallery = document.getElementById("gallery");
+  const errHTML = `
+    <div id="error">
+      <p>Oops! Looks like we had an issue loading this content!</p>
+    </div>
+  `;
+  gallery.innerHTML = errHTML;
+}
+
+// WINDOW EVENT LISTENER ====================================
 
 window.onload = function() {
-  getDataFromServer(usersUrl)
+  fetch(usersUrl)
+    .then(handleResponse)
     .then(data => generateGalleryCards(data.results))
     .then(data => attachCardListener(data))
     .then(handleSeachFeature)
-    .catch(err => console.log(Error(err)));
+    .catch(error => {
+      console.error(error);
+      renderErrorMessage();
+    });
 };
